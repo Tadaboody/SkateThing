9710bacc973a078902efdc12f0842cd789b56f54
Tomer Keren <tomer.keren.dev@gmail.com>
2020-01-27 22:58:23 +0200

    Js works now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-function main(){
+function main() {
     const canvas = document.getElementById("canvas")
+    const context = canvas.getContext("2d")
+    context.drawRect(100, 100, 100, 100)
+    context.fillStyle = "#FF0000"
+    context.fill()
 }
+
+main()
