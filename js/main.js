2524d1c4205aee97e572c82af90123a929d13bff
Tomer Keren <tomer.keren.dev@gmail.com>
2020-02-03 22:52:37 +0200

    Add achtung-like movement

--- js/main.js
+++ js/main.js
@@ -5,22 +5,60 @@ import { Vector2, clamp, Matrix2x2 } from "./engino/engino/Utilities.js";
 import Camera from "./engino/engino/Nodes/Camera.js";
 
 
-export class Player extends Node {
+class Player extends Node {
+    constructor(left, right, color, ...args) {
+        super(...args)
+        this.left = left
+        this.right = right
+        this.color = color
+        this.movementSpeed = 100
+        this.direction = 0
+        this.controlModifier = 1
+    }
+
     ready() {
-        this.sprite = new Rectangle(new Vector2(10, 10), "#FFFFFF")
+        this.sprite = new Rectangle(new Vector2(10, 10), this.color)
         this.addChild(this.sprite)
-        this.camera = new Camera()
-        this.addChild(this.camera)
-        this.camera.setActive()
+    }
+
+    update(deltatime) {
+        const deltaTimeInSec = deltatime / 1000;
+        if (this.inputManager.isDown('a')) {
+            this.direction -= deltaTimeInSec * this.controlModifier
+        }
+        if (this.inputManager.isDown('d')) {
+            this.direction += deltaTimeInSec * this.controlModifier
+        }
+
+        const scaledMoveSpeed = deltaTimeInSec * this.movementSpeed
+        const movementDirection = Matrix2x2.rotate(this.direction).apply(new Vector2(1, 0))
+        const movementDelta = movementDirection.mul(scaledMoveSpeed)
+        this.position = this.position.add(movementDelta);
+        this.rotation = this.direction
     }
 }
 
+// class Map extends Node {
+//     ready() {
+//         this.grid = []
+//     }
+
+//     /// param
+//     addSkatedPoint(point) {
+
+//     }
+
+// }
+
 class MainScene extends YOrderedGroup {
     ready() {
-        this.player = new Player()
+        this.player = new Player('a', 'd', '#FFFFFF')
         // this.block = new Rectangle(new Vector2(100, 30), 'gold', new Vector2(300, 300), Math.PI / 8);
         // this.addChild(this.block);
         this.addChild(this.player);
+        this.camera = new Camera()
+        this.addChild(this.camera)
+        this.camera.setActive()
     }
 }
 
